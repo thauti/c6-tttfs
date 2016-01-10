c171e41a95f02109707e5de9d29c8ce7425787be
HAUTIER Thomas <hautier@informatique.univ-paris-diderot.fr>
2016-01-10 13:50:21 +0100

    free_block

--- libtfs/tfs.c
+++ libtfs/tfs.c
@@ -15,7 +15,7 @@ int main()
 	disk_id d = malloc(sizeof(disk_id_s));
 	start_disk("truc.tfs", &d);
 	printf(" ====> %d\n",getvolumedeb(d,0));
-	add_free_block(d,0, 6);
+	add_free_block(d,0, 7);
 	stop_disk(d);
 	return 0;
 }
@@ -28,29 +28,35 @@ int add_free_block(disk_id d,int volume, int b)
 	read_block(d, blk,off/1024);
 	printf(" -> %d  ",blk[0]);
 	int nf = data_to_int(blocksub(blk,12),4);
-
 	printf("nombre block ! %d \n", nf);
-	//ajouter_infos(blk, 12, toLittleEndian(nf+1), 4);
+	ajouter_infos(blk, 12, toLittleEndian(nf+1), 4);
+
 	int ff = data_to_int(blocksub(blk,16),4);
-	printf("premier blokc! %d \n", ff);/*
+	printf("premier blokc! %d \n", ff);
+
 	if(ff == 0)
 	{
-		ajouter_infos(blk, 16, toLittleEndian(b), 4);
+		ajouter_infos(blk, 16, toLittleEndian(b), sizeof(toLittleEndian(b)));
 	}
 	else
 	{
-		int courant = ff;
-		read_block(d, aux, off+(1024*ff));
+		int suivant = ff;
+		read_block(d, aux, (off+(1024*ff))/1024);
 		int ln = data_to_int(blocksub(aux,1020),4);
-		while(ln != courant)
+		while(ln != suivant)
 		{
-			courant = ln;
-			ln = data_to_int(blocksub(aux,1020),4);
-			read_block(d, aux, off+(1024*ln));
+			ln = suivant;
+			suivant = data_to_int(blocksub(aux,1020),4);
+			block aux = malloc(BLOCK_SIZE);
+			printf("Lecture du block : encours: %d suivant: %d\n", ln, suivant);
+			read_block(d, aux, (off+(1024*ln))/1024);
 			
 		}
-		ajouter_infos(blk, 1020, toLittleEndian(b), 4);
-	}*/
+		write_block(d,blk,off/1024);
+		ajouter_infos(aux, 1020, toLittleEndian(b), sizeof(toLittleEndian(b)));
+		write_block(d, aux, (off+(1024*ln))/1024);
+		printf("ecriture sur le block %d \n de la valeur %d", (off+(1024*ln))/1024, b);
+	}
 	return 0;
 }
 int getvolumedeb(disk_id d, int part)
